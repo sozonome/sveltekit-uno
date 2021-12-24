4b3e7a6d252c14f0003363798d5d02e3db4cd110
Agustinus Nathaniel <berserk.nate@gmail.com>
2021-12-24 23:59:55 +0700

    chore: setup project

---
+++ src/lib/components/layout/Footer.svelte
@@ -0,0 +1,1 @@
+<footer layout p="4">{new Date().getFullYear()} | sznm.dev</footer>
---
+++ src/lib/components/layout/Header.svelte
@@ -0,0 +1,7 @@
+<header layout p="4">
+	<a href="/">
+		<h1 text="xl sm:3xl">sveltekit-uno</h1>
+	</a>
+
+	<p italic>SvelteKit template with unocss setup</p>
+</header>
